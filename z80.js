28e398ad60489c77e37b71dfece48fb32f8099a5
Matt Gibney <moxyfruzy@googlemail.com>
2019-04-26 20:54:20 +0100

    Basic changes required to make this work in a test environment.

--- z80.js
+++ z80.js
@@ -1,41 +1,36 @@
-
 const Z80JS = require("z80js");
-var fs = require('fs');
 
-var binary = fs.readFileSync('../test/bin/main.bin', 'hex');
+class Z80Memory {
+  constructor() {
+    this.memoryObj = {
+      '0': 0x00
+    };
+  }
 
-let program = binary.match(/.{2}/g);
+  read(address) {
+    return this.memoryObj[address] || 0x00;
+  }
 
-program = program.map(byte => parseInt(`0x${byte}`));
-console.log(program)
+  write(address, data) {
+    this.memoryObj[address] = data;
+  }
+}
 
-const memortStart = 0;
-const memory = {};
-
-let memoryPos = memortStart;
-program.forEach(byte => {
-  memory[memoryPos] = byte;
-  memoryPos = memoryPos + 1;
-});
-
-
+const memory = new Z80Memory();
 
 
 const z80 = Z80JS({
-  mem_read: (address) => {
-    // console.log("READ", address.toString(16));
-    return memory[address] || 0x00;
-  },
-  mem_write: (address, data) => {
-    // console.log("WRITE", address, data);
-  },
+  mem_read: (address) => memory.read(address),
+  mem_write: (address, data) => memory.write(address, data),
   io_read: () => {},
   io_write: () => {}
 });
 
 // Safety to stop program counter running away. For big applications, this may need to be increased.
-while(z80.getState().pc < 10000) {
-  console.log(z80.getState().a);
-  z80.run_instruction();
-  // console.log('----');
-}
+// while(z80.getState().pc < 10000) {
+//   // console.log(z80.getState().a);
+//   z80.run_instruction();
+//   // console.log('----');
+// }
+
+module.exports = { z80, memory };
